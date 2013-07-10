d484739b220ef1099da9695568f44477ad9badb0
Nathan Strauser <nathan.strauser@gmail.com>
2013-07-09 23:29:21 -0400

    typo

--- package.js
+++ package.js
@@ -3,7 +3,13 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.add_files('lib/boostrap-colorpicker/js/bootstrap-colorpicker.js', 'client');
-	api.add_files('lib/boostrap-colorpicker/css/bootstrap-colorpicker.css', 'client');
-	api.add_files('path-override.css', 'client');
+    api.add_files([
+        "lib/boostrap-colorpicker/js/bootstrap-colorpicker.js",
+        "lib/boostrap-colorpicker/css/bootstrap-colorpicker.css",
+        "lib/boostrap-colorpicker/img/bootstrap-colorpicker/alpha.png",
+        "lib/boostrap-colorpicker/img/bootstrap-colorpicker/hue.png",
+        "lib/boostrap-colorpicker/img/bootstrap-colorpicker/saturation.png"
+    ], "client");
+
+    api.add_files('path-override.css', 'client');
 });
